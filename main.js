ccb8a680bc1cc23529e76c37d921fa8bcaeb3ffa
dwyrwas <djwyrwas@gmail.com>
2022-06-19 19:46:21 -0400

    rewrote js file

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-let xTurn = true;
+/*let xTurn = true;
 let oTurn = false;
 
 function handleCellClick(e){
@@ -34,7 +34,28 @@ const boxes = document.querySelectorAll('div.box');
 boxes.forEach(element => {
     element.addEventListener('click', handleCellClick(e))
     })
-});
+}); 
+*/
 
+let gameActive = true;
+let currentPlayer = "X";
+let gameState = ['','','','','','','','',''];
+const winningMessage = () => `Player ${currentPlayer} has won!`;
+
+function handleCellPlayed(){
+
+}
+
+function handlePlayerChange(){
+
+}
+
+function handleResultValidtion(){
+
+}
+
+function handleCellClick(){
+
+}
 
 
